dcec75de449ad2a30d5ff8680f31de67f1b1c041
丁诚昊 <dimchumhaul@163.com>
2019-06-02 16:50:48 +0800

    2131231232

---
+++ Demos/Algorithm.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Algorithm.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("时间管理单元 Demo!");
+
+            var t = DateTime.Now;
+            var tuc = new TimeManageUnit
+            {
+                SegOneStartTime = TimeSpan.FromTicks(DateTime.Now.Ticks),
+                SegTwoStartTime = TimeSpan.FromTicks(DateTime.Now.AddSeconds(3).Ticks)
+            };
+            bool OK = tuc.IsTimeInSegOne(t);
+
+            // 微软进行了 全自动的 时间戳转换10进制算法
+            var v1 = t.AddMinutes(3);
+            var v2 = t.AddMinutes(3.9);
+            var NextHour = t.AddHours(1.5);
+        }
+    }
+}
