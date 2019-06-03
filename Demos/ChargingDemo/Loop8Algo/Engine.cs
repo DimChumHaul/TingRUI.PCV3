4f9957935062bf688fd03877cb3ecfba053a1356
丁诚昊 <dimchumhaul@163.com>
2019-06-04 02:28:50 +0800

    完成 两段式临停的 前面 33% 太极两仪生八卦

--- Demos/ChargingDemo/Loop8Algo/Engine.cs
+++ Demos/ChargingDemo/Loop8Algo/Engine.cs
@@ -8,8 +8,8 @@ using System.Threading.Tasks;
 namespace ChargingDemo.Loop8Algo
 {
     // 内核碎片函数指针 交给应用层调用者注入 设计模式：函数式编程+依赖注入
-    public delegate double IoC4ControversyHandlerV01(List<Tuple<DateTime,DateTime>> tParams, bool okTogo = false);
-    public enum 两仪 { 阴 = 3 << 8,阳 = 7 >> 6 }
+    public delegate double IoC4ControversyHandler(List<Tuple<DateTime,DateTime>> tParams, bool okTogo = false);
+    public enum 太极 { 阳 = 4 << 7 , 阴 = 6 << 8 }
 
     /// <summary>
     /// 算法状态机 ：八阵图 底层用到的算法逻辑封装类
@@ -18,21 +18,28 @@ namespace ChargingDemo.Loop8Algo
     {
         public string EngineToken { get; } = $"[没有规则] - 请继承自这个类并提交你需要的规则代码在子类的方法重写中";
         public TimeSpan EngineDead { get; set; }
-        public 两仪 起始时段 { get; set; } = 两仪.阳;
         public DateTime InTime { get; set; }
         public DateTime OutTimme { get; set; }
         public byte[] RuleCode { get; set; }
+
         #region 以下变量适用于所有规则
+        public 太极 起始时段 { get; set; } = 太极.阳;
         public int F1 { get; set; } = 10;
-        public Tuple<float, int> CubeSeg1 { get; set; } = new Tuple<float, int>(5.0f, 20);
-        public Tuple<float, int> CubeSegN { get; set; } = new Tuple<float, int>(2.0f, 120);
+
+        /* 白天盒子 */
+        public Tuple<float, int> CubeSun { get; set; } = new Tuple<float, int>(5.0f, 20);
+        /** 月光盒子 */
+        public Tuple<float, int> CubeMoon { get; set; } = new Tuple<float, int>(2.0f, 120);
         #endregion
 
         #region 算法引擎内核
         /* 算法底层所依赖的核心数据结构:【矩阵革命】微软Tuple数据结构切片儿 创建者：丁诚昊 v0.2 */ 
-        public List<Tuple<int,string,float>> MatrixRevolution;
         internal protected double TotalResult { get; protected set; }
-        protected internal Lazy<Dictionary<string, IoC4ControversyHandlerV01>> 争议处理办法;
+        /// <summary>
+        /// 参数列表 1.计费单元 2.计费规则 3.单元价格
+        /// </summary>
+        public List<Tuple<int,string,float>> Tailer = new List<Tuple<int, string, float>>();
+        protected internal Lazy<Dictionary<string, IoC4ControversyHandler>> 争议处理办法;
         #endregion
 
         public Engine(string RuleName = @"ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽヽ｀ヽ、ヽ｀ヽ｀、ヽ｀｀、ヽ 、｀｀、 ｀、ヽ｀ 、｀ ヽ｀ヽ、ヽ ｀、ヽ｀｀、ヽ、｀｀、｀、ヽ｀｀、 、ヽヽ｀、｀、、ヽヽ、｀｀、 、 ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽ、｀｀ヽ｀、、｀ヽ｀")
@@ -41,7 +48,7 @@ namespace ChargingDemo.Loop8Algo
             EngineDead = TimeSpan.FromSeconds(1.618);
             // 传入规则名称初始化
             if (!String.IsNullOrEmpty(RuleName)) EngineToken = RuleName;
-            争议处理办法 = new Lazy<Dictionary<string, IoC4ControversyHandlerV01>>();
+            争议处理办法 = new Lazy<Dictionary<string, IoC4ControversyHandler>>();
             争议处理办法.Value.Add("彭总的解决办法", (timesegments, OK) => 
             {
                 OK = true;
--- Demos/ChargingDemo/Loop8Algo/EngineIMPL/Seg2Engine.cs
+++ Demos/ChargingDemo/Loop8Algo/EngineIMPL/Seg2Engine.cs
@@ -14,58 +14,90 @@ namespace ChargingDemo.Loop8Algo.EngineIMPL
     public class Seg2Engine : Engine
     {
         public Seg2Engine(string RuleName) : base(RuleName) { }
-        // 一天中的小时数
-        public virtual Tuple<int, int> H1 { get; set; }
-        public virtual Tuple<int, int> H2 { get; set; }
+        /// 一天中的时间段 精确到秒 但这里就用整形 .NET SDK可以完成全自动转化  
+        /// 0.5天 = 720分 = 43200秒
+        /// 1天   = 1440分 = 86400秒
+        public Tuple<int, int> Seg1 { get; set; }
+        public Tuple<int, int> Seg2 { get; set; }
+
         // 后续所有单元默认使用第二段收费规则 如果用户勾选则选择第一段
         public bool UseSeg2 = true;
+
+        public void Time2Money(double TTM , 太极 SunSet = 太极.阳) 
+        {
+            // 1.阴阳画断
+            var CUBE = SunSet == 太极.阳 ? CubeSun : CubeMoon;
+            // 2.矩阵平方
+            int divides = (int)TTM / CUBE.Item2;
+            double tailer = TTM % CUBE.Item2;
+            // 3.辗转相除
+            for (int i = 0; i < divides; i++)
+            {
+                TotalResult += CUBE.Item1;
+                /// 参数列表 1.计费单元 2.计费规则 3.单元价格
+                Tailer.Add(new Tuple<int, string, float>(CUBE.Item2, EngineToken,CUBE.Item1));
+            }
+            // 处理【尾巴算法】只记录 不计价
+            Tailer.Add(new Tuple<int, string, float>((int)tailer, EngineToken + $"尾巴时间[{tailer}]分钟", CUBE.Item1));
+        }
 
         public override double CalculationIMPL(DateTime t1, DateTime t2, bool LetGo = false)
         {
             // 以下算法的逻辑 建立在 `最小计费单元是分钟`的基础上 不代表不可以继续切割分钟为更小的时间单元
             if (t1.Year != t2.Year) throw new NotImplementedException("跨年算法暂时不公开...");
             if (t1 > t2) throw new InvalidOperationException("出场时间必须大于入场时间");
-            if (H1.Item2 >= H2.Item1) throw new ArgumentException("两段式设置不允许有时间轴交集");
+            if (Seg1.Item2 >= Seg2.Item1) throw new ArgumentException("两段式设置不允许有时间轴交集");
 
-            var TTM = Math.Ceiling(Math.Abs((t2 - t1).TotalMinutes));
+            double TTM = Math.Abs((t2 - t1).TotalMinutes);
             bool isFree2Go = (int)TTM <= base.F1;
-            this.TotalResult = -0.0f;
-            this.InTime = t1 > t2 ? t2 : t1;
-            base.OutTimme = t1 < t2 ? t1 : t2;
+            TotalResult = -0.0f;
+            InTime = t1 > t2 ? t2 : t1;
+            OutTimme = t1 < t2 ? t1 : t2;
             // v1版本的两段式算法的逻辑为 (x / y * (times1 - 1)) + tailer1 + (z / c * (times2 - 1)) + tailer2
 
             // 开始计费 1.寻找起始点 
             if (isFree2Go) return TotalResult;
-            if (t1.Hour >= H1.Item1 && t1.Hour <= H1.Item2)
+            // 白天停车
+            if (t1.Hour >= Seg1.Item1 && t1.Hour <= Seg1.Item2)
             {
-                // 白天停车
-                起始时段 = 两仪.阳;
-                if (UseSeg2)
+                // 不跨时段
+                if (t2 <= DateTime.Today.AddHours(Seg1.Item2))
                 {
-
+                    Time2Money(TTM, 太极.阳);
+                    return TotalResult ;
                 }
-                else
+                // 跨时段
+                if (UseSeg2)
                 {
                     /* 如果超过当日让用户选择 超出第一天的部分 按照何种规则计价: 第一天夜间规则还是第一(二)天白天规则 */
                 }
+                else
+                {
+                    // type == 3(1---2---1---)
+                }
             }
-            else if (t1.Hour >= H2.Item1 && t1.Hour <= H2.Item2)
+            // 夜间停车 
+            else if (t1.Hour >= Seg2.Item1 && t1.Hour <= Seg2.Item2)
             {
-                // 夜间停车 
-                起始时段 = 两仪.阴;
+                // 不跨时段
+                if (t2 <= DateTime.Today.AddHours(Seg2.Item2))
+                {
+                    Time2Money(TTM, 太极.阴);
+                    return TotalResult;
+                }
+                // 跨时段
                 if (UseSeg2)
                 {
-
+                    /* 如果超过当日让用户选择 超出第一天的部分 按照何种规则计价: 第一天夜间规则还是第一(二)天白天规则 */
                 }
                 else
                 {
-
+                    // type == 3(1---2---1---)
                 }
-                /* 如果超过当日让用户选择 超出第一天的部分 按照何种规则计价: 第一天夜间规则还是第一(二)天白天规则 */
             }
             else
             {
-                // 白天和晚上各有1分钟的缓冲区
+                TotalResult = 0.0d; // 白天和晚上各有1分钟的缓冲区
             }
             return TotalResult;
         }
