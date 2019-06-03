9feeaea10aa990864e75fba66cb8f647fc3088ef
丁者诚昊 <dingkang3@DESKTOP-BREJPDP>
2019-06-03 11:40:45 +0800

    创建架构 :`计费规则引擎`

---
+++ Demos/ChargingDemo/CalculationModule/Engine.cs
@@ -0,0 +1,59 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingDemo.CalculationModule
+{
+    /// <summary>
+    /// 算法状态机 ：八阵图 底层用到的算法逻辑封装类
+    /// </summary>
+    internal class Engine : IChargeEngine
+    {
+        public string EngineName { get; } = $"[没有规则] - 请继承自这个类并提交你需要的规则代码在子类的方法重写中";
+        protected internal byte[] RuleCode { get; set; }
+        protected internal DateTime RuleValideUntil { get; set; }
+
+        #region 以下变量适用于所有规则
+        /// <summary>
+        /// 以下所有计费规则都以:`分钟`为`最小时间单元`
+        /// 免费时间6分钟，2元/18分钟，时段满收费。
+        /// </summary>
+        public int F1 { get; set; } = 6;
+        public int T1 { get; set; } = (int)Decimal.Zero;
+        public double T1Price { get; set; } = (double)Decimal.Zero  ;
+        /// after time unit per Unit Price 后续计费单元 例如 2元/18分钟  AnN = 18 
+        public int ANUnit { get; set; } = 2;
+        public double ANUPrice { get; set; } = 18.0d;
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        #endregion
+
+        public Engine(string RuleName , DateTime ValidDtime)
+        {
+            RuleValideUntil = ValidDtime == 
+                DateTime.MinValue ? DateTime.Now.AddYears(3) 
+                : ValidDtime;
+            if (!String.IsNullOrEmpty(RuleName))
+            {
+                EngineName = RuleName;
+            }
+        }
+
+        public virtual double CalculationPrice(DateTime InTime, DateTime OutTime, bool OKToLetGo = false)
+        {
+            return -0.0d;
+        }
+
+        public virtual string GenerateOrderDetail(List<Tuple<DateTime, DateTime, string>> TimeSlice)
+        {
+            var result = this.ToSafeJson();
+            var code = Encoding.UTF8.GetBytes(result);
+            this.RuleCode = code;
+            return result;
+        }
+    }
+}
---
+++ Demos/ChargingDemo/CalculationModule/IChargeEngine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingDemo.CalculationModule
+{
+    /// <summary>
+    /// 适用于人类一切`计费规则`相关的计费引擎接口
+    /// </summary>
+    interface IChargeEngine
+    {
+        /// <summary>
+        /// 计费规则名称
+        /// </summary>
+        string EngineName { get; }
+
+        /// <summary>
+        /// 计费规则引擎 必须提供一种核心算法 规则算法 与 应用层行业 是一对多的关系 
+        ///     例如 酒店管理行业就默认一种计费规则：夜审机制 
+        ///     再例如 停车软件 可能存在多种`基于时间轴`的计费规则 必须一一实现该接口 一种规则一个接口实现类
+        /// </summary>
+        /// <param name="InTime">入场时间</param>
+        /// <param name="OutTime">出场时间</param>
+        /// <param name="OKToLetGo">是否放行通过(完成支付)</param>
+        /// <returns>浮点数:订单合计价</returns>
+        double CalculationPrice(DateTime InTime, DateTime OutTime, bool OKToLetGo = false);
+
+        /// <summary>
+        /// 输出规则明细(停车消费的所有流水清单和合计价格) 优惠卷+减免 应该再增加一个API单独计算
+        /// </summary>
+        /// <param name="TimeSlice">时间轴切片(切片+RuleName)</param>
+        /// <param name="CharingRuleName">计费规则列表 与时间轴切片应该是一对一的关系</param>
+        /// <returns>输出每一种计费规则:`消费清单 + 合计金额 + 消费优惠` </returns>
+        string GenerateOrderDetail(List<Tuple<DateTime,DateTime,string>> TimeSlice);
+    }
+}
