28169af850ba07e507278a23ef0b9ff97487e2c3
丁诚昊 <dimchumhaul@163.com>
2019-06-04 01:27:57 +0800

    启动架构

--- Demos/ChargingDemo/Loop8Algo/Engine.cs
+++ Demos/ChargingDemo/Loop8Algo/Engine.cs
@@ -18,34 +18,24 @@ namespace ChargingDemo.Loop8Algo
     {
         public string EngineToken { get; } = $"[没有规则] - 请继承自这个类并提交你需要的规则代码在子类的方法重写中";
         public TimeSpan EngineDead { get; set; }
-        protected internal byte[] RuleCode { get; set; }
-
-        #region 以下变量适用于所有规则
+        public 两仪 起始时段 { get; set; } = 两仪.阳;
         public DateTime InTime { get; set; }
         public DateTime OutTimme { get; set; }
-        /// <summary>
-        ///     以下所有计费规则都以:`分钟`为`最小时间单元`
-        ///     免费时间6分钟，2元/18分钟，时段满收费。
-        /// </summary>
+        public byte[] RuleCode { get; set; }
+        #region 以下变量适用于所有规则
         public int F1 { get; set; } = 10;
-        public virtual Tuple<DateTime, DateTime> H1 { get; set; }
-        public virtual Tuple<DateTime, DateTime> H2 { get; set; }
-        // 最小计价单元: 分钟(M)inutes
         public Tuple<float, int> CubeSeg1 { get; set; } = new Tuple<float, int>(5.0f, 20);
         public Tuple<float, int> CubeSegN { get; set; } = new Tuple<float, int>(2.0f, 120);
-
-        protected internal Lazy<Dictionary<string, IoC4ControversyHandlerV01>> 争议处理办法;
         #endregion
 
         #region 算法引擎内核
         /* 算法底层所依赖的核心数据结构:【矩阵革命】微软Tuple数据结构切片儿 创建者：丁诚昊 v0.2 */ 
         public List<Tuple<int,string,float>> MatrixRevolution;
-        internal protected double Result { get; private set; }
-        // V0.1 - 仿制安快的算法数据结构 ：步长跳动算法
-        private Int32 CurrentTPivot { get; set; }
+        internal protected double TotalResult { get; protected set; }
+        protected internal Lazy<Dictionary<string, IoC4ControversyHandlerV01>> 争议处理办法;
         #endregion
 
-        public Engine(string RuleName = "ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽヽ｀ヽ、ヽ｀ヽ｀、ヽ｀｀、ヽ 、｀｀、 ｀、ヽ｀ 、｀ ヽ｀ヽ、ヽ ｀、ヽ｀｀、ヽ、｀｀、｀、ヽ｀｀、 、ヽヽ｀、｀、、ヽヽ、｀｀、 、 ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽ、｀｀ヽ｀、、｀ヽ｀")
+        public Engine(string RuleName = @"ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽヽ｀ヽ、ヽ｀ヽ｀、ヽ｀｀、ヽ 、｀｀、 ｀、ヽ｀ 、｀ ヽ｀ヽ、ヽ ｀、ヽ｀｀、ヽ、｀｀、｀、ヽ｀｀、 、ヽヽ｀、｀、、ヽヽ、｀｀、 、 ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽ、｀｀ヽ｀、、｀ヽ｀")
         {
             //var RuleValidaTime = DateTime.Now.AddYears(3);
             EngineDead = TimeSpan.FromSeconds(1.618);
--- Demos/ChargingDemo/Loop8Algo/EngineIMPL/Seg2Engine.cs
+++ Demos/ChargingDemo/Loop8Algo/EngineIMPL/Seg2Engine.cs
@@ -14,10 +14,60 @@ namespace ChargingDemo.Loop8Algo.EngineIMPL
     public class Seg2Engine : Engine
     {
         public Seg2Engine(string RuleName) : base(RuleName) { }
+        // 一天中的小时数
+        public virtual Tuple<int, int> H1 { get; set; }
+        public virtual Tuple<int, int> H2 { get; set; }
+        // 后续所有单元默认使用第二段收费规则 如果用户勾选则选择第一段
+        public bool UseSeg2 = true;
 
         public override double CalculationIMPL(DateTime t1, DateTime t2, bool LetGo = false)
         {
-            return -0.0d;
+            // 以下算法的逻辑 建立在 `最小计费单元是分钟`的基础上 不代表不可以继续切割分钟为更小的时间单元
+            if (t1.Year != t2.Year) throw new NotImplementedException("跨年算法暂时不公开...");
+            if (t1 > t2) throw new InvalidOperationException("出场时间必须大于入场时间");
+            if (H1.Item2 >= H2.Item1) throw new ArgumentException("两段式设置不允许有时间轴交集");
+
+            var TTM = Math.Ceiling(Math.Abs((t2 - t1).TotalMinutes));
+            bool isFree2Go = (int)TTM <= base.F1;
+            this.TotalResult = -0.0f;
+            this.InTime = t1 > t2 ? t2 : t1;
+            base.OutTimme = t1 < t2 ? t1 : t2;
+            // v1版本的两段式算法的逻辑为 (x / y * (times1 - 1)) + tailer1 + (z / c * (times2 - 1)) + tailer2
+
+            // 开始计费 1.寻找起始点 
+            if (isFree2Go) return TotalResult;
+            if (t1.Hour >= H1.Item1 && t1.Hour <= H1.Item2)
+            {
+                // 白天停车
+                起始时段 = 两仪.阳;
+                if (UseSeg2)
+                {
+
+                }
+                else
+                {
+                    /* 如果超过当日让用户选择 超出第一天的部分 按照何种规则计价: 第一天夜间规则还是第一(二)天白天规则 */
+                }
+            }
+            else if (t1.Hour >= H2.Item1 && t1.Hour <= H2.Item2)
+            {
+                // 夜间停车 
+                起始时段 = 两仪.阴;
+                if (UseSeg2)
+                {
+
+                }
+                else
+                {
+
+                }
+                /* 如果超过当日让用户选择 超出第一天的部分 按照何种规则计价: 第一天夜间规则还是第一(二)天白天规则 */
+            }
+            else
+            {
+                // 白天和晚上各有1分钟的缓冲区
+            }
+            return TotalResult;
         }
     }
 }
