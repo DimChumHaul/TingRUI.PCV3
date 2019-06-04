cc9b7fbaf68395c7a63cc6bb07f39cfdda588362
丁者诚昊 <dingkang3@DESKTOP-BREJPDP>
2019-06-04 11:17:09 +0800

    适配了 `二段式规则`的单元测试参数 用1440分钟以内的当日停车模拟停车场抬杆操作 以获取测试时间段

--- Demos/ChargingDemo/Program.cs
+++ Demos/ChargingDemo/Program.cs
@@ -26,25 +26,29 @@ namespace ChargingDemo
             var EndTime = T1.AddMinutes(NMinutes);
             $"停车总时长[{NMinutes}]分钟 起始时间[{T1}],结束时间[{EndTime}]".PrintDump();
 
-            DateTime t1Start = DateTime.Now.MakeTime(07, 00, 00);
-            DateTime t1End = DateTime.Now.MakeTime(19, 00, 00);
-            DateTime t2Start = t1End.AddMinutes(1);
-            // 注意这里是第二天早上6点
-            DateTime t2End = t1Start.AddDays(1);
-
-            // 生成规则+引擎计算 
-            IChargeEngine instance = new Seg2Engine("二段式收费")
             {
-                // 模拟`二段式收费`
-                Segment1 = new Tuple<DateTime, DateTime>(t1Start, t1End),
-                Segment2 = new Tuple<DateTime, DateTime>(t2Start, t2End),
-                CubeSun = new Tuple<float, int>(5.0f, 20),
-                CubeMoon = new Tuple<float, int>(2.0f, 120),
-                CrossNightRule = Loop8Algo.Enum.太极.阴,
-                FreeSeg1 = 10, // 免费时间10分钟 设置为0为没有免费时间
-            };
-            instance.CalculationIMPL(T1,EndTime);
-            instance.PrintDump();
+                DateTime t1Start = DateTime.Now.MakeTime(07, 00, 00);
+                DateTime t1End = DateTime.Now.MakeTime(19, 00, 00);
+                DateTime t2Start = t1End.AddMinutes(1);
+                /* 注意这里是第二天早上6点 有一个跨天的处理逻辑 */
+                DateTime t2End = t1Start.AddDays(1);
+                {
+                    // 生成规则+引擎计算 
+                    IChargeEngine instance = new Seg2Engine("二段式收费")
+                    {
+                        // 模拟`二段式收费`
+                        Segment1 = new Tuple<DateTime, DateTime>(t1Start, t1End),
+                        Segment2 = new Tuple<DateTime, DateTime>(t2Start, t2End),
+                        CubeSun = new Tuple<float, int>(5.0f, 20),
+                        CubeMoon = new Tuple<float, int>(2.0f, 120),
+                        CrossNightRule = Loop8Algo.Enum.太极.阴,
+                        FreeSeg1 = 10, // 免费时间10分钟 设置为0为没有免费时间
+                    };
+                    instance.CalculationIMPL(T1,EndTime);
+                    instance.PrintDump();
+                }
+            }
+
         }
     }
 }
