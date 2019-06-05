99a4ac714a8c4c59a85c506a7a677d162264fc67
丁诚昊 <dimchumhaul@163.com>
2019-06-06 00:36:55 +0800

    By 丁诚昊 职业技能考核的6个等级 Level6Pro

---
+++ Data.OfflineAPI.TingRUI/JustEnum/ProLevel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TingRUI.Data.JustEnum
+{
+    // By 丁诚昊 职业技能考核的6个等级 Level6Pro
+    public enum Level6Pro
+    {
+        // ---- 滚蛋 ---- 
+        菜鸟 = -32,
+        知道 = 32,
+        了解 = 8192,
+        // ---- 打杂 ---- 
+        熟练 = 65536,
+        // ---- 扶摇直上 ---- 
+        精通 = 1920*1080,
+        // ---- 融会贯通 ~> 发明创造 ----
+        修仙 = int.MaxValue
+    }
+}
