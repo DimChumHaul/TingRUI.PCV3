51f60cda7fdbc3b8f86b54a03b6f1efcf5298b24
丁诚昊 <dimchumhaul@163.com>
2019-06-08 02:03:34 +0800

    把尾巴算法和几家结果集全部拿到 抽象实体`天蝎座`中去

--- Demos/ChargingDemo/Loop8Algo/EngineV1.cs
+++ Demos/ChargingDemo/Loop8Algo/EngineV1.cs
@@ -25,14 +25,23 @@ namespace AlgoCore.Loop8Algo
     /// </summary>
     public class EngineV1 : IChargeEngine
     {
-        #region 脏代码区
-        public string EngineToken { get; private set; } = $"[没有规则]-请继承自这个类并提交你需要的规则代码在子类的方法重写中";
-        // 引擎计算容错预留 ：1.6秒没有完成 计费API+订单流水API 则写入崩溃日志
-        public const string 收费规则占位符 = "酒神代ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽヽ｀ヽ、ヽ｀ヽ｀、ヽ｀｀、ヽ 、｀｀、｀、ヽ｀ 、｀ ヽ｀ヽ、ヽ ｀、ヽ｀｀、ヽ、｀｀、｀、ヽ｀｀、 、ヽヽ｀、｀、、ヽヽ、｀｀、 、 ヽ｀、ヽ｀｀、ヽ｀ヽ｀、、ヽ ｀ヽ 、ヽ｀｀ヽ、｀｀ヽ｀、、｀ヽ｀谷雨代";
-        public TimeSpan EngineWorkTimeout { get; set; } 
-        protected internal byte[] RuleCode { get; set; }
-        public string Billing = "【停如意】- 错峰停车首创者 停车场管理无人值守引领者" + Environment.NewLine;
-        public EngineV1(string RuleName = 收费规则占位符)
+        #region 内核引擎 - v1.0 参数区
+        // 免费时长? 单位:分钟
+        public int FreeTime { get; set; } = 60;
+        /* 北京/成都 向上向下取整 ~ */
+        public FloorOrCeil floorOrCeil = FloorOrCeil.Ceil;
+        // 出入场时间
+        public DateTime InTime { get; set; }
+        public DateTime OutTime { get; set; }
+
+        /* 盒子模型 附加规则 = [二段式盒子 + 潮汐盒子 + 24H盒子 + 单次盒子 + ... 各种盒子 */
+        public List<(DateTime start, DateTime end, TingRUICube RuiCube)> TinCubes { get; set; }
+        private DKSkyScorpion _scorpion;
+        // 记录作业引擎中的重要操作 转化为字符串 记录到内存中 将来写入内核日志
+        public IEnumerable<string> EngineLog { get; set; }
+        #endregion
+
+        public EngineV1(string RuleName,List<ValueTuple<DateTime,DateTime,TingRUICube>> RuleList)
         {
             // 0.传入规则名称初始化
             if (!string.IsNullOrEmpty(RuleName))
@@ -40,31 +49,11 @@ namespace AlgoCore.Loop8Algo
                 EngineToken = RuleName;
                 EngineWorkTimeout = TimeSpan.FromSeconds(1.618);
             }
-            // 1-1.初始化 作业引擎【时间切片儿】容器 - Init
-            Tail = new List<ValueTuple<double, decimal?, bool?>> { };
             // 1-2.初始化 规则引擎【规则盒子】容器 - Init
-            TinCubes = new List<ValueTuple<DateTime, DateTime, TingRUICube>>();
+            TinCubes = RuleList;
         }
-        #endregion 
 
-        #region 内核引擎 - v1.0 参数区
-        // 免费时长?
-        public int FreeSeg1 { get; set; } = 60;
-        /* 向上向下取整 ~ */
-        public FloorOrCeil floorOrCeil = FloorOrCeil.Ceil;
-        public DateTime InTime { get; set; }
-        public DateTime OutTime { get; set; }
-
-        public decimal? TotalResult { get; protected internal set; } = -.0m;
-        /* 尾巴儿容器 参数列表 1.计费单元 2.计费规则 3.单元价格 */
-        public List<(double ttmUnit, decimal? unitPrice, bool? discount)> Tail { get; set; }
-        /* 盒子模型 附加规则 = [二段式盒子 + 潮汐盒子 + 24H盒子 + 单次盒子 + ... 各种盒子 */
-        public List<(DateTime start, DateTime end, TingRUICube RuiCube)> TinCubes { get; set; }
-        // 记录作业引擎中的重要操作 转化为字符串 记录到内存中 将来写入内核日志
-        public IEnumerable<string> EngineLog { get; set; }
-        #endregion
-
-        #region Algorithm's Core IMPL
+        #region 内核引擎 - Algorithm's Core IMPL
         // TTM 时间轴校对函数
         public virtual bool ParamCheck(DateTime T1, DateTime T2)
         {
@@ -77,75 +66,75 @@ namespace AlgoCore.Loop8Algo
         }
 
         /// <summary>
-        ///【停如意错峰算法】：作业函数: 
-        ///     时间轴划界 + 含附加规则算法簇 + ⏳盒子m³模型 + "尾巴"算法 + "借元"算法
-        ///     v1版本不解决`跨年`问题
+        ///【停如意错峰算法】:
+        ///     丁诚昊作业蝎子 + ⏳停如意盒子m³模型 + "尾巴"算法 + "借元"算法 + 依赖注入争议函数指针
+        ///     v1版本不解决`跨年`问题 以天为最大计费单元 以分钟为最小计费单元
         /// </summary>
         /// <param name="tStart">真正的停车起始时间 (不包含各种时间减免) 用于确认时间轴的下界 </param>
         /// <param name="tEnd">出场时间 时间轴上届 包含"借元"算法 </param>
         /// <param name="letGo">是否直接开闸放行</param>
         /// <returns>钱.订单详情列表.时间片信息组</returns>
-        public decimal? EngineGo(DateTime tStart, DateTime tEnd, bool letGo = false)
+        internal decimal? EngineGo(DateTime tStart, DateTime tEnd, bool letGo = false)
         {
-            if (TinCubes.Count() <= 0) throw new Exception("基准规则寻址失败...规则清单长度为0...");
-            if (letGo) throw new NotImplementedException("直接开闸放行...做好日志处理和权限操作记录");
-            
-            /* 0-1.启动三重作业引擎: 作业蝎子 + 盒子模型 * 矩阵平方 * 尾巴儿算法 */
-            TotalResult = -.0m;
-            var PIVOT = new DKScorpio(tStart,tEnd);
-            while(PIVOT.PivotLeft >= tStart && PIVOT.PivotRight < tEnd )
-            {
-                // 我跟夏老师吹牛逼 我可以写一个算法不用一行If Else 但还是用了..但实际上是可以放到While()中的而不用if的
-                if (letGo == false) break;
+            // 检查参数
+            ParamCheck(tStart, tEnd);
 
+            // 直接开闸放行...做好日志处理和权限操作记录
+            _scorpion = new DKSkyScorpion(tStart,tEnd);
+            if (letGo) 
+            {
+                _scorpion.TotalResult = 5.0m;
+                return _scorpion.TotalResult;
+            }
+            if (TinCubes.Count() <= 0) throw new Exception("基准规则寻址失败...规则清单长度为0...");
+
+            while(_scorpion.PivotLeft < tEnd && _scorpion.PivotRight < tEnd )
+            {
                 // 规则附加(投放盒子) * 时间片切割(矩阵平方) * 循环划界切割最小单元(for)
-                for (int ruleIdx = 0; ruleIdx < TinCubes.Count(); ruleIdx++)
+                for (int idx = 0; idx < TinCubes.Count(); idx++)
                 {
                     // 1-1.启动时间轴引擎 开始附加基础规则
-                    var Cube = TinCubes[ruleIdx].RuiCube as TingRUICube;
-
+                    TingRUICube Cube = TinCubes[idx].RuiCube;
                     // 1-2.释放丁诚昊蝎子 
-                    var ptu = Cube.PTU; var mtu = Cube.MTU; var ppm = Cube.PPM;
+                    var ptu = Cube.PTU; // price per unit
+                    var mtu = Cube.MTU; // minutes per unit
+                    var ppm = Cube.PPM; // price per minute
                     var ruleStart = Cube.RuleStart;
                     var ruleEnd = Cube.RuleEnd;
                     var disRate = Cube.DisRate;
                     var enableDis = Cube.EnableRuleCuoFeng;
-
-                    // 1-3.蝎子摆尾 定位单次规则内的左右手时间点 (注意已经进入作业流水线内层While循环)
-                    PIVOT.PivotLeft  = tStart > ruleStart.FromTime2DT() ? tStart : ruleStart.FromTime2DT(); 
-                    PIVOT.PivotRight = tEnd > ruleEnd.FromTime2DT()? ruleEnd.FromTime2DT() : tEnd;
-
+                    // 1-3.蝎子摆尾 定位单次规则内的左右手时间点 (左右手不断++)
+                    _scorpion.PivotLeft  = tStart >= ruleStart.FromTime2DT() ? tStart : ruleStart.FromTime2DT();
+                    _scorpion.PivotRight = tEnd <= ruleEnd.FromTime2DT() ? tEnd : ruleEnd.FromTime2DT();
                     // 2-2.矩阵平方
-                    var minutes = (int)(PIVOT.PivotRight - PIVOT.PivotLeft).TotalMinutes;
+                    var minutes = (int)(_scorpion.PivotRight - _scorpion.PivotLeft).TotalMinutes;
                     var Counts = minutes / mtu;
                     var Rest = minutes % mtu;
-
-                    // 3-1.万佛朝宗(递归获取大盒子内的小盒子个数 辗转相加累计营收)
+                    // 3-1.循环作业获取大盒子内的小盒子个数 辗转相加 保存每一段时间片儿计费
                     for (int index = 0; index < Counts; index++)
                     {
                         // 在争议得不到解决的情况下 我先进行`精准计算` 精确到每一分钟(小数点后32位)
-                        TotalResult += ptu * (decimal)(enableDis ? disRate : 1);
+                        _scorpion.TotalResult += ptu * (decimal)(enableDis ? disRate : 1);
                         // 记录每一个带`色彩(规则)`的小盒子
-                        Tail.Add((mtu,ptu,enableDis));
+                        _scorpion.Tail.Add((mtu,ptu,enableDis));
                     }
                     // 3-2.尾巴儿算法&容器数据结构
                     var RestMoney = ppm * Rest * (enableDis ? disRate : 1);
-                    TotalResult += (decimal)RestMoney;
-                    Tail.Add((Rest, (decimal)RestMoney, Cube.EnableRuleCuoFeng));
-
-                    // 5.蝎子进行下一次作业循环 挪动左右手 重新获取时间轴边界
-                    ; ; ;
+                    _scorpion.TotalResult += (decimal)RestMoney;
+                    _scorpion.Tail.Add((Rest, (decimal)RestMoney, Cube.EnableRuleCuoFeng));
                 }
-                InTime = tStart;
-                OutTime = tEnd;
+                // 鞋子左右手完成所有时间片切割则跳出死循环
+                _scorpion.PivotRight = _scorpion.PivotLeft = tEnd;
             }
-            return TotalResult;
+            InTime = tStart; OutTime = tEnd;
+            return _scorpion.TotalResult;
         }
+        #endregion
 
         /* IMPL = implementation 算法的实现 */
         public virtual decimal? CalculationIMPL(DateTime t1, DateTime t2, bool LetGo = false)
         {
-            return -.0m;
+            return this.EngineGo(t1,t2, LetGo);
         }
 
         /* IMPL = implementation 算法的实现 */
@@ -156,6 +145,14 @@ namespace AlgoCore.Loop8Algo
             RuleCode = code;
             return $"订单:[{Guid.NewGuid()}] - \n\t 停车收费: ---|{result}|--- ";
         }
-        #endregion
+
+        #region 脏代码区
+        public string EngineToken { get; private set; } = $"[没有规则]-请继承自这个类并提交你需要的规则代码在子类的方法重写中";
+        
+        // 引擎计算容错预留 ：1.6秒没有完成 计费API+订单流水API 则写入崩溃日志
+        public TimeSpan EngineWorkTimeout { get; set; }
+        protected internal byte[] RuleCode { get; set; }
+        public string Billing = "【停如意】- 错峰停车首创者 停车场管理无人值守引领者" + Environment.NewLine;
+        #endregion 
     }
 }
