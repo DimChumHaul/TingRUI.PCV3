bc73e2a55835f0a70b2aaeee30619230d8d2e349
丁诚昊 <dimchumhaul@163.com>
2019-06-08 23:44:00 +0800

    343242343

--- Data.OfflineAPI.TingRUI/JustEnum/ProLevel.cs
+++ Data.OfflineAPI.TingRUI/JustEnum/ProLevel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace TingRUI.Data.JustEnum
 {
     // By 丁诚昊 职业技能考核 Pro6Level
-    public enum Level6Pro
+    public enum Level6Pro 
     {
         // ---- 滚蛋 ---- 
         菜鸟 = -32, 知道 = 32, 了解 = 8192,
@@ -15,19 +10,18 @@ namespace TingRUI.Data.JustEnum
         熟练 = 65536,
         // ---- 扶摇直上 ---- 
         精通 = 1920*1080,
-        // ---- 融会贯通 | 发明创造 ----
+        // ---- 融会贯通 | 发明创造 ---- 
         修仙 = int.MaxValue
     }
 
     // 丁诚昊 六级人才分割理论
-    public enum NormalPeople
+    public enum Level6People 
     {
         // ---- 滚蛋 ---- 
-        废才,庸才,人才,
-        // ---- 哈哈 ----
+        庸才, 废才, 人才,
         // 巧言令色 恩威并用 收揽人心 为我所用 发现了第一个真正的`将才`
-        将才,
-        天才 = 1027 * 768,
-        众神之王 
+        将才 = 1024 * 768, 天才 = 1024, 鬼才 = 768,
+        // 奥丁,灭霸,成吉思汗
+        众神之王 = int.MaxValue
     }
 }
