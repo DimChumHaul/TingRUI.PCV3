c174c90ead802157350cf907f33684221fa94ced
丁者诚昊 <dingkang3@DESKTOP-BREJPDP>
2019-06-26 17:24:48 +0800

    下班前最后一次提交

--- Src/PCChageTermialV3.TingRUI/ModelViews/MainWindow.xaml.cs
+++ Src/PCChageTermialV3.TingRUI/ModelViews/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace PCChageTermialV3.TingRUI
 {
@@ -36,26 +37,22 @@ namespace PCChageTermialV3.TingRUI
             {
                 // 初始化首页[顶部模组]
                 LoadBasicUI();
-
                 // 注册MVVMLight消息机订阅模式 上层模式：观察者 底层模式:static函数指针区插值
                 Messenger.Default.Register<string>(this, "TopViewTokenAction", BtnSelectedChange);
             };
-
             this.Unloaded += (sender, e) => {
                 // 卸载当前(this)对象注册的所有MVVMLight消息
                 Messenger.Default.Unregister(this);
             };
         }
-
         #region 哈哈  用了TabControl之后甚至都没走这个方法 不过这段Demo留着 有用
         private void BtnSelectedChange(string Token)
         {
             UserSelectingIndex = Convert.ToInt32(Token);
             if (string.IsNullOrEmpty(Token)) throw new ArgumentOutOfRangeException("MvvmLight消息机Token非法~");
-
             /*
             * MVVM Light Messenger 
-            * 旨在通过简单的设计模式来精简此场景：任何对象都可以是接收端；
+            * 旨在通过简单的设计模式来精简此场景:任何对象都可以是接收端;
             * 任何对象都可以是发送端；任何对象都可以是消息 
             */
             string originTitle = this.Title;
@@ -65,24 +62,20 @@ namespace PCChageTermialV3.TingRUI
             TopView.SelectedIndex = UserSelectingIndex;
         }
         #endregion
-
         private void LoadBasicUI()
         {
             // 1.找到应该加载的 首页TopView 索引数
             UserSelectingIndex = 3;
-
-            // 2.异步|同步 ...加载页面... 
+            // 2.异步|同步 ...加载页面...
         }
-
         private void LaunchMahAppsOnGitHub(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/MahApps/MahApps.Metro");
+            Process.Start("https://github.com/MahApps/MahApps.Metro");
         }
         private void LaunchIcons(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/MahApps/MahApps.Metro.IconPacks");
+            Process.Start("https://github.com/MahApps/MahApps.Metro.IconPacks");
         }
-
         private int UserSelectingIndex { get; set; }
     }
 }
